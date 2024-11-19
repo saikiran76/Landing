10f28a3ad19adb8c07232ddd41ee9d7bf69b43a5
gopalksahoo063 <gopal@havahavai.com>
2024-11-19 14:13:37 +0530

    hero section has been crafted

--- saas/starter-template/src/components/Button.tsx
+++ saas/starter-template/src/components/Button.tsx
@@ -1,4 +1,4 @@
-import { HTMLAttributes } from "react";
+import { ButtonHTMLAttributes } from "react";
 import { cva } from "class-variance-authority";
 
 
@@ -7,12 +7,17 @@ const classes = cva('border h-12 rounded-full px-6 font-md', {
         variant:{
             primary:'bg-lime-400 text-neutral-950 border-lime-400',
             secondary:'border-white text-white bg-transparent'
+        },
+        size:{
+            sm:'h-10',
+            md:'text-md',
+            lg:'text-lg'
         }
     }
 
 })
 
-export default function Button(props: { variant: "primary" | "secondary" } & HTMLAttributes<HTMLButtonElement>) {
-    const {variant, className, ...rest} = props;
-    return <button className={classes({variant:variant, className:className})} {...rest} />
+export default function Button(props: { variant: "primary" | "secondary", size?: "sm" | "md" | "lg" } & ButtonHTMLAttributes<HTMLButtonElement>) {
+    const {variant, className, size, ...rest} = props;
+    return <button className={classes({variant:variant, size:size, className:className})} {...rest} />
 }
--- saas/starter-template/src/sections/Hero.tsx
+++ saas/starter-template/src/sections/Hero.tsx
@@ -1,3 +1,55 @@
+import Button from "@/components/Button";
+import designExampleImage from "@/assets/images/design-example-1.png";
+import designExampleImage2 from "@/assets/images/design-example-2.png";
+import Image from "next/image";
+
+import Pointer from "@/components/pointer";
+
 export default function Hero() {
-    return <div>Hero</div>;
+    return(
+    <section className="py-24 overflow-x-clip">
+        <div className="container relative">
+            <div className="absolute -left-32 top-16 hidden lg:block">
+                <Image src={designExampleImage} alt="Design Example 1" />
+            </div>
+            <div className="absolute -right-64 -top-16 hidden lg:block">
+                <Image src={designExampleImage2} alt="Design Example 2" />
+            </div>
+
+            <div className="absolute left-40 lg:top-80 md: top-36">
+                <Pointer name="Andrew" color="red" />
+            </div>
+
+            <div className="absolute right-40 -top-10">
+                <Pointer name="Gwen" color="blue" />
+            </div>
+            <div className="flex justify-center">
+                <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-500 rounded-full text-sm font-medium text-black">
+                    ✨Seeing myself grow each project
+                </div>
+            </div>
+
+            <h1 className="text-6xl md:text-7xl font-medium text-center mt-6">
+                Building my way to build frontends perfectly
+            </h1>
+
+            <p className="text-center text-xl max-w-2xl mx-auto text-white/60 mt-8">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. 
+            </p>
+
+            <form className="flex border border-white/15 rounded-full py-2 mt-8 lg:max-w-2xl mx-auto">
+                <input 
+                    type="email"
+                    placeholder="Enter your email"
+                    className="bg-transparent px-4 md:flex-1 w-full"
+                />
+                <Button type="submit" variant="primary" size="sm"className="whitespace-nowrap mr-5">
+                    Go
+                </Button>
+            </form>
+
+         </div>
+
+        
+    </section>)
 }
